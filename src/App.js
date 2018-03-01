f0fda1ebed9079d9527f11682b20df70b54b7b80
Aanand Prasad <aanand.prasad@gmail.com>
2018-03-01 15:25:47 +0000

    drop CSV file and parse

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Dropzone from "react-dropzone";
+import readData from "./read-data";
+
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.onDrop = this.onDrop.bind(this);
+    this.onReadFile = this.onReadFile.bind(this);
+  }
+
+  onDrop(acceptedFiles) {
+    const reader = new FileReader();
+    reader.onload = this.onReadFile;
+    reader.readAsText(acceptedFiles[0]);
+  }
+
+  onReadFile(event) {
+    const rawData = event.target.result;
+    const data = readData(rawData);
+    console.log(data);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Dropzone onDrop={this.onDrop} />
       </div>
     );
   }
