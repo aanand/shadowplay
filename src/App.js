dea4f8bc7e30c67a6422a2b844291a7eaa9e0ef1
Aanand Prasad <aanand.prasad@gmail.com>
2018-03-02 13:55:03 +0000

    clear button and welcome message

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
     this.onDrop = this.onDrop.bind(this);
     this.onReadFile = this.onReadFile.bind(this);
     this.onConfigChange = this.onConfigChange.bind(this);
+    this.clear = this.clear.bind(this);
 
     this.state = {
       data: null,
@@ -57,29 +58,64 @@ class App extends Component {
     this.setState({ config });
   }
 
+  clear() {
+    this.setState({ data: null });
+  }
+
   render() {
     const { data, config } = this.state;
     return (
       <div className="App" style={{ backgroundColor: config.backgroundColor }}>
         <div className="App-controls">
-          <div className="App-dropzone">
-            <Dropzone
-              className="App-dropzone-inner"
-              activeClassName="App-dropzone-inner-active"
-              onDrop={this.onDrop}
-            >
-              Drop tweets.csv here
-            </Dropzone>
-          </div>
+          {data ? (
+            <div className="App-clear">
+              <button type="button" onClick={this.clear}>
+                Start again
+              </button>
+            </div>
+          ) : (
+            <div className="App-dropzone">
+              <Dropzone
+                className="App-dropzone-inner"
+                activeClassName="App-dropzone-inner-active"
+                onDrop={this.onDrop}
+              >
+                Drop tweets.csv here
+              </Dropzone>
+            </div>
+          )}
 
           {data && (
             <ControlPanel config={config} onChange={this.onConfigChange} />
           )}
         </div>
 
-        {data && (
+        {data ? (
           <div className="App-chart">
             <Chart data={data} config={config} />
+          </div>
+        ) : (
+          <div className="App-welcome">
+            <div className="App-welcome-inner">
+              <h1>Shadowplay</h1>
+              <p>Make a nice graph of your Twitter activity.</p>
+              <ol>
+                <li>
+                  <b>Request your Twitter archive.</b> Go to “Settings and
+                  privacy”, scroll down and click “Request your archive”.
+                  Twitter will email you a link to a zip file.
+                </li>
+                <li>
+                  <b>Download the archive.</b>
+                </li>
+                <li>
+                  <b>Open it and find tweets.csv.</b>
+                </li>
+                <li>
+                  <b>Drop it over there on the left.</b>
+                </li>
+              </ol>
+            </div>
           </div>
         )}
       </div>
