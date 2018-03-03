45bb784817e2857d4b53ff750453c8b940e891a3
Aanand Prasad <aanand.prasad@gmail.com>
2018-03-03 18:51:02 +0000

    enhance mobile message

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,14 +5,15 @@ import "./Welcome.css";
 export default () => (
   <div className="Welcome">
     <div className="Welcome-inner">
-      <h1>Shadowplay</h1>
+      <h1>SHADOWPLAY</h1>
       <p>Make a nice graph of your Twitter activity.</p>
       <ol>
         <li>
           <b>Request your Twitter archive.</b> Go to
-          <a href="https://twitter.com/settings/account" target="_blank">“Settings
-          and privacy”</a>, scroll down and click “Request your archive”. Twitter
-          will email you a link to a zip file.
+          <a href="https://twitter.com/settings/account" target="_blank">
+            “Settings and privacy”
+          </a>, scroll down and click “Request your archive”. Twitter will email
+          you a link to a zip file.
         </li>
         <li>
           <b>Download the archive.</b>
